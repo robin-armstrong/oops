e5880f6a10c8cb36ddc4623d12d738eaff5c8fed
Robin Armstrong <rja243@cornell.edu>
2024-07-03 15:13:44 -0600

    QuadratureUpdate application now saves the analysis ensemble member before closing.

--- src/oops/runs/QuadratureUpdate.h
+++ src/oops/runs/QuadratureUpdate.h
@@ -63,6 +63,9 @@ class QuadratureUpdateParameters : public ApplicationParameters {
   /// Parameters for cost function used in initialization
   RequiredParameter<eckit::LocalConfiguration> finalConfig{"final", this};
 
+  /// Parameters for outputting the analysis increment
+  RequiredParameter<eckit::LocalConfiguration> outputConfig{"output", this};
+
   /// Parameters for cost function used in initialization
   RequiredParameter<eckit::LocalConfiguration> cfConfig{"cost function", this};
 };
@@ -124,15 +127,15 @@ template <typename MODEL, typename OBS> class QuadratureUpdate : public Applicat
     const JbTotal_ & Jb = J->jb();
 
 //  Get the forecast mean and model auxiliary information
-    CtrlVar_ x_bg(Jb.getBackground());
-    ModelAux_ & maux = x_bg.modVar();
-    ObsAux_ & oaux   = x_bg.obsVar();
+    CtrlVar_ x0(Jb.getBackground());
+    ModelAux_ & maux = x0.modVar();
+    ObsAux_ & oaux   = x0.obsVar();
 
-//  Setup post-processor
-    PostProcessor<State_> post;
+//  Setup post-processor for cost function evaluation
+    PostProcessor<State_> eval_post;
     if (iterconfs[0].has("prints")) {
       const eckit::LocalConfiguration prtConfig(iterconfs[0], "prints");
-      post.enrollProcessor(new StateInfo<State_>("traj", prtConfig));
+      eval_post.enrollProcessor(new StateInfo<State_>("traj", prtConfig));
     }
 
 /// "Dummy" evaluation of test function, which serves to properly initialize
@@ -140,141 +143,42 @@ template <typename MODEL, typename OBS> class QuadratureUpdate : public Applicat
 /// trying to initialize a ControlIncrement.
 
     iterconfs[0].set("linearize", true);
-    J->evaluate(x_bg, iterconfs[0], post);
+    J->evaluate(x0, iterconfs[0], eval_post);
     util::printRunStats("QuadratureUpdate linearize " + std::to_string(0));
 
 //  Setup independent geometry configuration object for reading ensemble members
     const Geometry_ geometry(params.cfConfig.value().getSubConfiguration("geometry"), this->getComm(), mpi::myself());
 
 //  Get the ensemble state to be updated
-    State4D_ x_ens_state(geometry, params.ensMemberConfig);
-    Log::test() << "Ensemble state: " << x_ens_state << std::endl;
+    State4D_ xens_bg_state(geometry, params.ensMemberConfig);
+    Log::test() << "Background ensemble state: " << xens_bg_state << std::endl;
 
 //  Wrapping the ensemble state in a control variable
-    std::shared_ptr<State4D_>  xens_ptr(&x_ens_state);
+    std::shared_ptr<State4D_>  xens_ptr(&xens_bg_state);
     std::shared_ptr<ModelAux_> maux_ptr(&maux);
     std::shared_ptr<ObsAux_>   oaux_ptr(&oaux);
-    CtrlVar_                   x_ens(xens_ptr, maux_ptr, oaux_ptr);
+    CtrlVar_                   xens_bg_ctrl(xens_ptr, maux_ptr, oaux_ptr);
 
-//  Taking difference between background and ensemble state to form the increment that will be transformed.
-    CtrlInc_ dx_in(Jb);
-    dx_in.diff(x_ens, x_bg);
+//  Taking difference between background and ensemble state to form the background increment.
+    CtrlInc_ dx_bg(Jb);
+    dx_bg.diff(xens_bg_ctrl, x0);
 
+//  Computing the analysis increment via numerical quadrature.
     QuadSolver_ quadsolver(*J);
-    CtrlInc_ dx_out = quadsolver.solve(dx_in, 20, 100, 1e-2);
-
-    std::cout << "DEBUGGING: Finished quadrature solve." << std::endl;
-
-//     std::cout << "DEBUGGING: normalizing by obs-error covariance." << std::endl;
-// //  Normalizing by the obs-error covariance.
-//     Dual_ dw_pre;
-//     R_invsqrt_mat.multiply(dy, dw_pre);
-
-//     std::cout << "DEBUGGING: running SLCG." << std::endl;
-//     std::vector<Dual_> X;
-//     std::vector<double> lambda; lambda.push_back(1);
-//     SLCG(X, NormalHBHt_mat, dw_pre, lambda, 1, 20, 1e-10);
+    int quadsize   = 20;
+    int maxiters   = 100;
+    double tol     = 1e-2;
+    CtrlInc_ dx_an = quadsolver.solve(dx_bg, quadsize, maxiters, tol);
     
+//  Add the analysis increment to the background, forming an analysis ensemble member.
+    CtrlVar_ xens_an_ctrl(x0);
+    J->addIncrement(xens_an_ctrl, dx_an);
+    State4D_ xens_an_state = xens_an_ctrl.states();
+    Log::test() << "Analysis ensemble state: " << xens_an_state << std::endl;
 
-// //  Perform Incremental Variational Assimilation
-//     eckit::LocalConfiguration varConf(fullConfig, "variational");
-//     int iouter = IncrementalAssimilation<MODEL, OBS>(xx, *J, varConf);
-//     Log::info() << "QuadratureUpdate: incremental assimilation done "
-//                 << iouter << " iterations." << std::endl;
-
-// //  Save analysis and final diagnostics
-//     PostProcessor<State_> post;
-//     if (fullConfig.has("variational output")) {
-//       const eckit::LocalConfiguration outConfig(fullConfig, "variational output");
-//       post.enrollProcessor(new StateWriter<State_>(outConfig));
-//     }
-
-//     eckit::LocalConfiguration finalConfig(fullConfig, "final");
-//     finalConfig.set("iteration", iouter);
-
-// //  Save increment if desired
-//     if (finalConfig.has("increment")) {
-//       const eckit::LocalConfiguration incConfig(finalConfig, "increment");
-//       ControlVariable<MODEL, OBS> x_b(J->jb().getBackground());
-//       const eckit::LocalConfiguration incGeomConfig(incConfig, "geometry");
-//       Geometry<MODEL> incGeom(incGeomConfig,
-//                               xx.states().geometry().getComm(),
-//                               xx.states().commTime());
-//       ControlIncrement<MODEL, OBS> dx_tmp(J->jb());
-//       ControlIncrement<MODEL, OBS> dx(incGeom, dx_tmp);
-//       dx.diff(xx, x_b);
-//       const eckit::LocalConfiguration incOutConfig(incConfig, "variational output");
-//       dx.write(incOutConfig);
-//     }
-
-//     if (finalConfig.has("increment to structured grid")) {
-//       const eckit::LocalConfiguration incLatlonConf(finalConfig, "increment to structured grid");
-
-//       ControlVariable<MODEL, OBS> x_b(J->jb().getBackground());
-//       ControlIncrement<MODEL, OBS> dx(J->jb());
-//       dx.diff(xx, x_b);
-
-//       const StructuredGridWriter<MODEL> latlon(incLatlonConf, dx.states().geometry());
-//       for (size_t jtime = 0; jtime < dx.states().size(); ++jtime) {
-//         latlon.interpolateAndWrite(dx.states()[jtime], xx.states()[jtime]);
-//       }
-//     }
-
-//     if (finalConfig.has("prints")) {
-//       const eckit::LocalConfiguration prtConfig(finalConfig, "prints");
-//       post.enrollProcessor(new StateInfo<State_>("final", prtConfig));
-//     }
-
-//     if (finalConfig.has("analysis to structured grid")) {
-//       const eckit::LocalConfiguration anLatlonConf(finalConfig, "analysis to structured grid");
-//       post.enrollProcessor(new StructuredGridPostProcessor<MODEL, State_>(
-//             anLatlonConf, xx.state().geometry() ));
-//     }
-
-//     J->evaluate(xx, finalConfig, post);
-
-// //  Save ObsAux
-//     xx.obsVar().write(cfConf);
-
-//     if (finalConfig.has("forecast from analysis")) {
-//       const eckit::LocalConfiguration fcFromAnConf(finalConfig, "forecast from analysis");
-
-//       //  Setup Model
-//       const Model_ model(xx.state().geometry(), eckit::LocalConfiguration(fcFromAnConf, "model"));
-
-//       //  Setup augmented state
-//       const ModelAux_ moderr(xx.state().geometry(),
-//                             eckit::LocalConfiguration(fcFromAnConf, "model aux control"));
-
-//       //  Setup times
-//       const util::Duration fclength(fcFromAnConf.getString("forecast length"));
-//       const util::DateTime bgndate(xx.state().validTime());
-//       const util::DateTime enddate(bgndate + fclength);
-//       Log::info() << "Running forecast from " << bgndate << " to " << enddate << std::endl;
-
-//       //  Setup forecast outputs
-//       PostProcessor<State_> post;
-
-//       eckit::LocalConfiguration prtConfig;
-//       if (fcFromAnConf.has("prints")) {
-//         prtConfig = eckit::LocalConfiguration(fcFromAnConf, "prints");
-//         post.enrollProcessor(new StateInfo<State_>("fc", prtConfig));
-//       }
-
-//       eckit::LocalConfiguration outConfig;
-//       if (fcFromAnConf.has("variational output")) {
-//         outConfig = eckit::LocalConfiguration(fcFromAnConf, "variational output");
-//         outConfig.set("date", bgndate.toString());
-//         post.enrollProcessor(new StateWriter<State_>(outConfig));
-//       }
-//       //  Run forecast
-
-//       Log::test() << "Inital state: " << xx.state() << std::endl;
-
-//       model.forecast(xx.state(), moderr, fclength, post);
-
-//       Log::test() << "Final state: " << xx.state() << std::endl;
-//     }
+//  Save analysis ensemble member
+    eckit::LocalConfiguration outConfig = params.outputConfig.value();
+    xens_an_state.write(outConfig);
 
     util::printRunStats("QuadratureUpdate end");
     Log::trace() << "QuadratureUpdate: execute done" << std::endl;
